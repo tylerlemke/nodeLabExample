6af01a3834ff5d4b3aef2699ac8746f299f2c6ca
tylerlemke <lemke164@umn.edu>
2015-01-22 13:51:19 -0600

    Changed kitten message and 404 message

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/kittens', function(req, res){
-    res.send("Page under construction!");
+    res.send("Sorry, We're all out of Kittens!");
 });
 
 app.get('/about', function(req, res){
@@ -57,7 +57,7 @@ app.get('/', function(req, res){
 
 // Wildcard catches requests for non-existent routes or files and responds with a 404 message (or an html page if you want to make a custom one!)
 app.get('*', function(req, res){
-    res.send("404");
+    res.send("Sorry, page doesn't exist...yet");
 //    res.sendFile('404.html', options);
 });
 
